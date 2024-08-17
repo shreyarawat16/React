6fc6bdbc6779b27edd482463e7da995744045682
shreyarawat16 <shreyarawat363@gmail.com>
2024-08-17 19:16:42 +0530

    first

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+
+function App() {
+  return (
+    <h1> Chai aur react | SHREYA RAWAT </h1>
+  );
+}
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  
+  return (
+   <h1>Chai aur React with Vite | Shreya Rawat</h1>
+  )
+}
+
+export default App
